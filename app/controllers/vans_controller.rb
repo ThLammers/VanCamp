11f37282fb429c341acded05a374b3f4690b1b3e
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-21 13:39:55 +0200

    add easy paramerter to check if search was triggered

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,13 +1,18 @@
 class VansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :search]
 
   def index
-    if params[:location] || params[:seats]
+    if params[:commit] == "search"
       seats = search_params[:seats]
       location = search_params[:location]
-      @vans = Van.where(location: location && :seats >= seats)
+      @vans = Van.where('location ILIKE :location AND seats >= :seats', location: @location, seats: seats)
     else
       @vans = Van.all
     end
+  end
+
+  def search
+    # raise
   end
 
   private
