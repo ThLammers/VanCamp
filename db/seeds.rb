783d2f8e6bd4a22b6136ad7e20c318424dba64ce
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-21 15:12:50 +0200

    add testuser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@ Booking.destroy_all
 Van.destroy_all
 User.destroy_all
 
+# create testuser (will be our admin later)
+User.create!(first_name: "admin", last_name: "admin", password: "123456", email:"admin@admin.com", date_of_birth: Date.new(1990,5,1))
+
 # create random instances
 create_users(10)
 create_vans(100)
