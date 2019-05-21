96256ae7e320efb70f90a18209840f220b10694a
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-21 18:33:07 +0200

    improve forms and add playground page for testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :search]
+  skip_before_action :authenticate_user!, only: [:home, :thomas]
   def home
     # @user = current_user
+  end
+
+  def thomas
+    @users = User.all
+    @vans = Van.all
+    @bookings = Booking.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get '/thomas', to: "pages#thomas" # temporary playground for DEVELOPMENT only !!!
+
   get 'vans/search', to: "vans#search"
   resources :vans do
     resources :bookings
