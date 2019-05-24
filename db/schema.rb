da85afb89f3a6bb2a0711a1a6a0c8c7e03fabf6e
Assunta Walderdorff <assunta@walderdorff.org>
2019-05-24 12:51:49 +0200

    add user references to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_085402) do
+ActiveRecord::Schema.define(version: 2019_05_24_104947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_05_24_085402) do
     t.bigint "van_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
     t.index ["van_id"], name: "index_reviews_on_van_id"
   end
 
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_085402) do
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "vans"
+  add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "vans"
   add_foreign_key "vans", "users"
 end
